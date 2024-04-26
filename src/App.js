f8fb1c40247d0178274c025ccf2990d0aef87f3c
Alieva Nargiza <nargiza.alieva018@gmail.com>
2024-04-27 02:53:31 +0600

    added cart list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Cart from "./components/Cart";
-import person1 from "./img/person1.jpeg"
+import React from "react";
+import CartList from "./components/cart_list/CartList";
+
 
 function App() {
+  
 
   return (
     <div className="App">
-      <Cart name="Alia" surname="Alia" image={person1} dateOfBirth="01/01/2005" dateOfMissing="12/12/2002" />
+      <CartList />
     </div>
   );
 }
